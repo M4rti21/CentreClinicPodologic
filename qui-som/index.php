ecd9ebe93fbb61f2c3aea4b9621fbad1b1018fa6
M4rti21 <m4rti21@proton.me>
2024-11-08 19:45:04 +0100

    actualitzar fotos

--- qui-som/index.php
+++ qui-som/index.php
@@ -1,97 +1,76 @@
 <?php include_once "../src/head.php"; ?>
 <title><?php echo ucwords(basename(__DIR__)); ?> | Centre Clinic Podològic</title>
 <?php include_once "../src/navbar.php"; ?>
-<div class="card person m-3">
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img src="../src/img/podologues/gloria.jpg"
-                class="img-fluid rounded-start ratio-1x1 podoFoto"
-                alt="..." />
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h1 class="card-title">Glòria Canal Sellarés</h1>
-                <hr />
-                <h4>Diplomada en Podologia</h4>
-                <h5>Universitat de Barcelona</h5>
-                <h6>Núm COP: 838080780</h6>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="card person m-3">
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img
-                src="../src/img/podologues/miriam.jpg"
-                class="img-fluid rounded-start ratio-1x1 podoFoto"
-                alt="..." />
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h1 class="card-title">Míriam Álvarez Camino</h1>
-                <hr />
-                <h4>Diplomada en Podologia</h4>
-                <h5>Fundació Universitària del Bages (FUB)</h5>
-                <h6>Núm COP: 8380801078</h6>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="card person m-3">
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img
-                src="../src/img/podologues/mariajose.jpg"
-                class="img-fluid rounded-start ratio-1x1 podoFoto"
-                alt="..." />
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h1 class="card-title">MªJosé Aldea Rodón</h1>
-                <hr />
-                <h4>Diplomada en Podologia</h4>
-                <h5>Universitat de Barcelona</h5>
-                <h6>Núm COP: 838080913</h6>
-            </div>
-        </div>
-    </div>
-</div>
+<?php 
+$podologues = array(
+  (object) [
+    'codi' => 'gloria',
+    'nom' => 'Glòria Canal Sellarés',
+    'atributs' => array(
+        "Diplomada en Podologia",
+        "Universitat de Barcelona",
+        "Núm COP: 838080780"
+    )
+  ],
+  (object) [
+    'codi' => 'miriam',
+    'nom' => 'Míriam Álvarez Camino',
+    'atributs' => array(
+        "Diplomada en Podologia",
+        "Fundació Universitària del Bages (FUB)",
+        "Núm COP: 8380801078"
+    )
+   ],
+  (object) [
+    'codi' => 'mariajose',
+    'nom' => 'MªJosé Aldea Rodón',
+    'atributs' => array(
+        "Diplomada en Podologia",
+        "Universitat de Barcelona",
+        "Núm COP: 838080913"
+    )
+   ],
+  (object) [
+    'codi' => 'silvia',
+    'nom' => 'Silvia Royo Pertusa',
+    'atributs' => array(
+        "Graduada en Podologia",
+        "Universitat de Barcelona",
+        "Núm COP: 8380802229"
+    )
+   ],
+  (object) [
+    'codi' => 'davinia',
+    'nom' => 'Davinia',
+    'atributs' => array(
+        "Responsable d'Administració i Recepció",
+    )
+   ],
+);
+
+foreach ($podologues as $p) {
+?>
 <div class="card person m-3">
     <div class="row g-0">
         <div class="col-md-4">
             <img
-                src="../src/img/podologues/silvia.jpg"
+            src="../src/img/podologues/<?php echo $p->codi ?>.jpg"
                 class="img-fluid rounded-start ratio-1x1 podoFoto"
                 alt="..." />
         </div>
         <div class="col-md-8">
             <div class="card-body">
-                <h1 class="card-title">Silvia Royo Pertusa</h1>
+            <h1 class="card-title"><?php echo $p->nom ?></h1>
                 <hr />
-                <h4>Graduada en Podologia</h4>
-                <h5>Universitat de Barcelona</h5>
-                <h6>Núm COP: 8380802229</h6>
+                <?php $i = 4 ?>
+                <?php foreach ($p->atributs as $a) { ?>
+                <h<?php echo $i ?>><?php echo $a ?></h4>
+                <?php $i++ ?>
+                <?php } ?>
             </div>
         </div>
     </div>
 </div>
-<div class="card person m-3">
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img
-                src="../src/img/podologues/caro.jpg"
-                class="img-fluid rounded-start ratio-1x1 podoFoto"
-                alt="..." />
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h1 class="card-title">Carolina</h1>
-                <hr />
-                <h4>Responsable d'Administració i Recepció</h4>
-            </div>
-        </div>
-    </div>
-</div>
+<?php } ?>
 </div>
 <?php include_once "../src/footer.php"; ?>
