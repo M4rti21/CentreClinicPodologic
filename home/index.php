72fd5ddc4bf17cbea81e884cea69c91e3cbd0db8
M4rti21 <m4rti21@proton.me>
2024-11-08 19:21:42 +0100

    update homescreen carrousel

--- home/index.php
+++ home/index.php
@@ -3,21 +3,14 @@
 <?php include_once "../src/navbar.php"; ?>
 <div class="carousel slide" data-bs-ride="carousel" id="carouselInici">
     <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="20000">
-            <img alt="..." class="d-block w-100" src="../src/img/carrusel/1.jpeg">
+<?php for ($i=1; $i <= 5; $i++) {  ?>
+        <div class="carousel-item <?php echo $i == 1 ? "active" : "" ?>" data-bs-interval="20000"
+            style="background-image: url('../src/img/carrusel/<?php echo $i ?>.jpeg'); background-repeat: no-repeat; background-position: center; background-size: cover;">
+            <div style="width: 100%; backdrop-filter: blur(10px); display: flex; justify-content: center;">
+                <img alt="..." class="d-block w-100" src="../src/img/carrusel/<?php echo $i ?>.jpeg" style="max-width: 600px">
+            </div>
         </div>
-        <div class="carousel-item" data-bs-interval="20000">
-            <img alt="..." class="d-block w-100" src="../src/img/carrusel/2.jpeg">
-        </div>
-        <div class="carousel-item" data-bs-interval="20000">
-            <img alt="..." class="d-block w-100" src="../src/img/carrusel/3.jpeg">
-        </div>
-        <div class="carousel-item" data-bs-interval="20000">
-            <img alt="..." class="d-block w-100" src="../src/img/carrusel/4.jpeg">
-        </div>
-        <div class="carousel-item" data-bs-interval="20000">
-            <img alt="..." class="d-block w-100" src="../src/img/carrusel/5.jpeg">
-        </div>
+<?php } ?>
     </div>
     <button class="carousel-control-prev" data-bs-slide="prev" data-bs-target="#carouselInici" type="button">
         <span aria-hidden="true" class="carousel-control-prev-icon"></span>
